f8878dc8ef0356ec4bee5da1fbf08600b3b22dbe
srinya2598 <srinya2598@gmail.com>
2019-04-14 23:16:39 +0530

    logout function added

--- src/app/shared/models/category.ts
+++ src/app/shared/models/category.ts
@@ -1,13 +1,13 @@
 export enum IProductCategory {
-  MOBILE_COMPUTER = "mobile and computer",
-  ELECTRONIC_APPLIANCES = "electronic appliances",
-  HOME_APPLIANCES = "home appliances",
-  MEN_CLOTHING = "men clothing",
-  WOMEN_CLOTHING = "women clothing",
-  KIDS_CLOTHING = "kids clothing",
-  TOYS = "toys",
-  VEHICLES = "vehicles",
-  BOOKS = "books",
-  MOVIES_MUSIC_VIDEOS = "movies music videos",
-  OTHERS = "others"
+  MOBILE_COMPUTER = 'mobile and computer',
+  ELECTRONIC_APPLIANCES = 'electronic appliances',
+  HOME_APPLIANCES = 'home appliances',
+  MEN_CLOTHING = 'men clothing',
+  WOMEN_CLOTHING = 'women clothing',
+  KIDS_CLOTHING = 'kids clothing',
+  TOYS = 'toys',
+  VEHICLES = 'vehicles',
+  BOOKS = 'books',
+  MOVIES_MUSIC_VIDEOS = 'movies music videos',
+  OTHERS = 'others'
 }
