423a5c6bf6b88e2356ded08802e63174bcb55553
manvi00 <imanvisrivastava@gmail.com>
2019-06-02 23:12:10 +0530

    chat-window ui

--- src/app/chat/chat-routing.ts
+++ src/app/chat/chat-routing.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { ChatscreenComponent } from './components/chatscreen/chatscreen.component';
+import {ChatWindowComponent} from './components/chat-window/chat-window.component';
 
 export const chatRoutes: Routes = [
   {
     path: '',
-    component: ChatscreenComponent,
+    component: ChatWindowComponent,
   }
 ];
--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -3,10 +3,12 @@ import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
 import { chatRoutes } from './chat-routing';
 import { ChatscreenComponent } from './components/chatscreen/chatscreen.component';
+import { ChatWindowComponent } from './components/chat-window/chat-window.component';
 
 @NgModule({
   declarations: [
     ChatscreenComponent,
+    ChatWindowComponent,
   ],
   imports: [
     SharedModule,
