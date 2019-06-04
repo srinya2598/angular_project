718132fc7a7c43a160dc1db6579183511e216987
naimishverma17 <naimishverma50@gmail.com>
2019-06-04 23:29:35 +0530

    removed duplicate bootstrap import

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,9 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { HttpClientModule } from '@angular/common/http';
-import { BootstrapComponent } from '../bootstrap/bootstrap.component';
 
 @NgModule({
-  declarations: [BootstrapComponent],
+  declarations: [],
   imports: [
     SharedModule,
     HttpClientModule
