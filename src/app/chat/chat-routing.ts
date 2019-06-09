c3bf1a9d4373a1227232adc676d849afeb2494bb
manvi00 <imanvisrivastava@gmail.com>
2019-06-09 23:28:49 +0530

    chat bubble ui

--- src/app/chat/chat-routing.ts
+++ src/app/chat/chat-routing.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { ChatscreenComponent } from './components/chatscreen/chatscreen.component';
 import {ChatWindowComponent} from './components/chat-window/chat-window.component';
+import {ChatBubbleComponent} from './components/chat-bubble/chat-bubble.component';
 
 export const chatRoutes: Routes = [
   {
--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -4,11 +4,13 @@ import { RouterModule } from '@angular/router';
 import { chatRoutes } from './chat-routing';
 import { ChatscreenComponent } from './components/chatscreen/chatscreen.component';
 import { ChatWindowComponent } from './components/chat-window/chat-window.component';
+import { ChatBubbleComponent } from './components/chat-bubble/chat-bubble.component';
 
 @NgModule({
   declarations: [
     ChatscreenComponent,
     ChatWindowComponent,
+    ChatBubbleComponent,
   ],
   imports: [
     SharedModule,
