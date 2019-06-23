22bd7dae87d186e911208ed11847cbdbf443e4a2
manvi00 <imanvisrivastava@gmail.com>
2019-06-23 16:42:29 +0530

    entity adapter created

--- src/app/chat/actions/message.ts
+++ src/app/chat/actions/message.ts
@@ -1,10 +1,12 @@
 import {Action} from '@ec-core/actions';
 import {IMessage} from '@ec-shared/models/message';
+import {IRoom} from '@ec-shared/models/room';
 
 export enum ChatActions {
 
   SEND_MESSAGE = '[chat] send message',
   FETCH_MESSAGE = '[chat] fetch message ',
+  FETCH_SUCCESS = '[chat] fetch success',
 
 }
 
@@ -20,6 +22,15 @@ export class FetchMessage implements Action {
   readonly type = ChatActions.FETCH_MESSAGE;
 
   constructor(public payload: IMessage[]) {
+
+  }
+
+}
+
+export class FetchSuccess implements Action {
+  readonly type = ChatActions.FETCH_SUCCESS;
+
+  constructor(public  payload: { room: IRoom[], roomId: string }) {
 
   }
 
---
+++ src/app/shared/models/room.ts
@@ -0,0 +1,4 @@
+export interface IRoom {
+  roomid: string;
+  participants: string[];
+}
