5f5374b086288e0b2dd8f4baaf61faaf8835500e
srinya2598 <srinya2598@gmail.com>
2019-06-30 22:53:39 +0530

    fetch selecteduUser details

--- src/app/chat/components/chat-window/chat-window.component.ts
+++ src/app/chat/components/chat-window/chat-window.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import {Component, Input, OnInit} from '@angular/core';
+import {FormControl} from '@angular/forms';
 import {ConversationalController} from '@ec-core/controllers/conversational.controller';
+import {IUser} from '@ec-shared/models/users';
+import {take} from 'rxjs/operators';
+import {ApiService} from '@ec-core/services/api.service';
 
 @Component({
   selector: 'app-chat-window',
@@ -8,12 +11,33 @@ import {ConversationalController} from '@ec-core/controllers/conversational.cont
   styleUrls: ['./chat-window.component.scss']
 })
 export class ChatWindowComponent implements OnInit {
-
+  @Input() selectedUser: IUser;
   message: FormControl;
   showSendMessageButton = false;
+  firstName: string;
+  lastName: string;
+  profileUrl: string;
+
 
   constructor(private conversationalController: ConversationalController) {
     this.message = new FormControl(null);
+
+    this.conversationalController.getSelectedUserId().pipe(take(1)).subscribe(id => {
+
+
+      this.apiService.getUserDetails(id).pipe(take(1)).subscribe((res: IUser) => {
+        console.log(res);
+
+        this.firstName = res.firstName;
+        this.lastName = res.lastName;
+        this.profileUrl = res.profileUrl;
+
+
+      });
+
+    });
+
+
   }
 
   ngOnInit() {
